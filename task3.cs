0be63a59003c97c2ff6cca28474ffc32d9ea51f7
KristinaSharaeva <insight.vld@gmail.com>
2022-06-27 00:58:26 -0300

    task3

---
+++ task3.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 0) {
+    Console.WriteLine("четное");
+} else {
+    Console.WriteLine("нечетное");
+}
