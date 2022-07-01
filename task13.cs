1d057fbcfaecf9df7a861ae44f259ecc29dd197c
KristinaSharaeva <insight.vld@gmail.com>
2022-06-30 23:35:55 -0300

    .

--- task13.cs
+++ task13.cs
@@ -1,4 +1,4 @@
-  Console.WriteLine ("Enter your number");
+ Console.WriteLine ("Enter your number");
     string number = Console.ReadLine ();
         if (number.Length < 3){
       Console.WriteLine ("there is no third number");
